0706068dd4cbeb3dfb3af91d94a9deb48872fe0d
Makfront0123 <ing.sistemas2019armando@gmail.com>
2025-01-09 22:49:32 -0400

    se agrega server cambios

--- server/server.js
+++ server/server.js
@@ -11,16 +11,13 @@ const app = express();
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['set-cookie'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-
-
-
-
-
-
 
 const routeFiles = fs.readdirSync('./routes');
 
